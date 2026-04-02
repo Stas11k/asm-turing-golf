4aee51af43bf11d7ab6718b0a7b117757145fc67
Stanislav <168761269+Stas11k@users.noreply.github.com>
2026-04-02 22:39:38 +0300

    feat: use PSP command line directly as ASCIIZ filename (#1)

--- tm.asm
+++ tm.asm
@@ -3,16 +3,21 @@
 ORG 100h
 
 start:
-    ; Write "HELLO" to stdout
-    mov ah, 40h         ; DOS: Write to file/device
-    mov bx, 1           ; stdout handle
-    mov cx, 5           ; 5 bytes
-    mov dx, offset msg
-    int 21h
+    mov si, 81h
 
-    mov ah, 4Ch         ; DOS: Terminate
-    int 21h
+skip_spaces:
+    lodsb
+    cmp al,' '
+    je skip_spaces
+    lea dx, [si-1]
 
-msg db 'HELLO'
+find_end:
+    lodsb
+    cmp al, 0Dh
+    jne find_end
+    mov byte ptr [si-1], 0
+
+    mov ah, 4Ch
+    int 21h
 
 END start
