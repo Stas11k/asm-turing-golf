7e649ebfed1fb70ed30477f898728b5eae2ecf57
Stanislav <168761269+Stas11k@users.noreply.github.com>
2026-04-02 23:48:33 +0300

    feat: open tm file and read it into buffer (#1)

--- tm.asm
+++ tm.asm
@@ -17,7 +17,19 @@ find_end:
     jne find_end
     mov byte ptr [si-1], 0
 
+    mov ah, 3Dh
+    int 21h
+    xchg ax, bx
+    mov ah, 3Fh
+    mov ch, 7Dh
+    mov dx, offset file_buf
+    int 21h
+    xchg ax, di
+    mov byte ptr [file_buf + di], 0
+
     mov ah, 4Ch
     int 21h
+
+file_buf label byte
 
 END start
