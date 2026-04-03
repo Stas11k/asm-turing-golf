8fb0275cfb3b15e7f3a87af719232ec0613a5e81
Stanislav <168761269+Stas11k@users.noreply.github.com>
2026-04-03 13:15:28 +0300

    feat: parse start and halt state names from header (#2)

--- tm.asm
+++ tm.asm
@@ -25,23 +25,36 @@ find_end:
     mov dx, offset file_buf
     int 21h
     xchg ax, di
-    mov byte ptr [file_buf + di], 0
+    mov [file_buf + di], bh
 
     mov si, offset file_buf
 
 find_header:
     lodsb
-    or al, al
+    cbw
+    test ax, ax
     jz done
     cmp al, ';'
     je skip_comment
     cmp al, 32
     jbe find_header
     dec si
+    mov di, offset start_name
+    call get_token
+    mov di, offset halt_name
     
-done:
-    mov ah, 4Ch
-    int 21h
+get_token:
+    lodsb
+    cmp al, ' '
+    jbe get_token
+    
+copy_loop:
+    stosb
+    lodsb
+    cmp al, ' '
+    ja copy_loop
+    mov [di-1], bh
+    ret
 
 skip_comment:
     lodsb
@@ -51,6 +64,12 @@ skip_comment:
     jne skip_comment
     jmp find_header
 
+done:
+    mov ah, 4Ch
+    int 21h
+
+start_name db 9 dup(0)
+halt_name  db 9 dup(0)
 file_buf label byte
 
 END start
